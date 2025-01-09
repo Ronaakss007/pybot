d2c0f2daf7dfc03e2fdc05d802287f48c29db414
Ronak <sainironak975@gmail.com>
2025-01-09 21:33:22 +0530

    start updated

--- plugins/start.py
+++ plugins/start.py
@@ -203,7 +203,7 @@ async def start_command(client: Client, message: Message):
                         [InlineKeyboardButton("Click Here 👆", url=link)],
                         [InlineKeyboardButton('How to open this link 👆', url=TUT_VID)]
                         ]
-                    await message.reply(f"Total clicks {clicks}. Here is your link 👇.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
+                    await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: 1 Day\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 hours after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
                     return
     
     for i in range(1):
